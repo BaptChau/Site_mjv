847fa3c632fc1bdc4609a3872a99599bcde79f2a
Baptiste Chaudron <chaudron964@gmail.com>
2019-11-22 10:43:02 +0100

    Ajout des routes et page pour babyhand

---
+++ views/mjv/equipes/babyhand.php
@@ -0,0 +1,24 @@
+<?php
+
+require 'C:\wamp64\www\sitePerso\vendor\autoload.php';
+
+
+?>
+
+
+<h1>Equipe BabyHand</h1>
+<div>
+
+<div class="container">
+    <div class="card border-primary mb-3" style="max-width: 20rem;">
+    <div class="card-header">Calendrier BabyHand</div>
+    <div class="card-body">
+        <p class="card-text">Il n'y a pas de calendrier ou de classement pour le baby hand ce sont des plateaux. Pour plus d'information demander au responsable de l'équipe</p>
+    </div>
+    </div>
+</div>
+    <h3>Le mot du responsable de l'equipe Vivien Raulet : </h3>
+    <p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim corporis eligendi cupiditate quidem accusantium provident, soluta natus ipsum minus officia, reprehenderit odio similique obcaecati pariatur labore inventore perferendis. Aspernatur, blanditiis.
+    </p>
+</div>
