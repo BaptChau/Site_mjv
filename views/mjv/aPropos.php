51da94c526653044b8a1c411db0e3048f95d2e38
Baptiste Chaudron <chaudron964@gmail.com>
2019-12-11 16:43:19 +0100

    fin de journée + a propos

--- views/mjv/aPropos.php
+++ views/mjv/aPropos.php
@@ -1,1 +1,30 @@
-<h1> Bonjour </h1>
+<div class="container">
+    <p>Le club a été crée en 1970 dans le village de vaubecourt et à tout de suite eu le nom que l'on connais</p>
+    <p>Ajourd'hui le club est présidé par Fabien Obara et la secrétaire et Catherine Michelot</p>
+    <p>Voici le programe des entrainement :</p>
+    <div class="container">
+        <table class="table table-bordered table-hover">
+            <thead class=" table-dark">
+                <tr>
+                    <td>
+                        Équipe :
+                    </td>
+                    <td>
+                        Coach :
+                    </td>
+                    <td>
+                        Horraire :
+                    </td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Sénior Masculin</td>
+                    <td>Luc Chaudron</td>
+                    <td>21h15-22h45</td>
+                    
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
