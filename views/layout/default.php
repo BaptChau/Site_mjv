a94b1652e51eacf67b425e3a2f238fb9aad0efe8
Baptiste Chaudron <chaudron964@gmail.com>
2019-12-18 15:08:31 +0100

    Résolution du probléme de soummission d'article venant du captcha

--- views/layout/default.php
+++ views/layout/default.php
@@ -1,5 +1,5 @@
 <?php
-// require '../../vendor/autoload.php';
+require '../vendor/autoload.php';
 if(!empty($_SESSION))
 // dump($_SESSION);
 
--- views/mjv/calepin/newArticle.php
+++ views/mjv/calepin/newArticle.php
@@ -1,8 +1,5 @@
 <?php
 require_once 'C:\wamp64\www\sitePerso\vendor\autoload.php';
-
-
-
 ?>
 <form action="<?= $router->url('articleValid', $_POST) ?>" method="post">
     <fieldset>
@@ -16,10 +13,5 @@ require_once 'C:\wamp64\www\sitePerso\vendor\autoload.php';
         <label for="contenu">Corps de l'article</label>
         <textarea name="contenu" require class="form-control" id="contenu" cols="30" rows="10" placeholder="Quel est l'horaire du match -18 de ce week-end ?"></textarea>
     </fieldset>
-    <div class="form-group">
-        <div class="g-recaptcha" data-sitekey="6LfoAMgUAAAAAHL6LfCJMKZBn3m3Tkl-_wbbalEX" data-callback="verifyRecaptchaCallback" data-expired-callback="expiredRecaptchaCallback"></div>
-        <input class="form-control d-none" data-recaptcha="true" required data-error="Please complete the Captcha">
-        <div class="help-block with-errors"></div>
-    </div>
-    <button type="submit" class="btn btn-primary btn-lg g-recaptcha">Envoyer</button>
+    <button type="submit" class="btn btn-primary btn-lg">Envoyer</button>
 </form>
